c2ab606a1fcb8443538ebdd2b519cc1b815e1e0e
Guy Segev <guysegev@crossrider.com>
2013-07-19 10:28:50 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LandingStrip::Application.config.secret_token = '1df6066fc9c116d6abfe5f4a894f40864a12fceb41d2dcf91631f712a382d2ef0bdb6e70e57db8bb4b7c02ebdfa65b24491fa04ca0780d8f534b8c782092d4fe'
